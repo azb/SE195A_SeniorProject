6b68fe9113349158c8bee9cf48b821ebbf24908b
Krystle <krystleweinrich@yahoo.com>
2016-10-28 21:41:22 -0700

    added Item, Listing Profile constructors

---
+++ IROM/app/src/main/java/com/sjsu/se195/irom/Listing.java
@@ -0,0 +1,19 @@
+package com.sjsu.se195.irom;
+
+import java.util.Date;
+
+/**
+ * Created by Krystle on 10/28/2016.
+ */
+
+public class Listing {
+    public String listID;
+    public Date dateCreated;
+    public String creator;
+    public Item item;
+    public String description;
+    public Boolean isLive;
+    public Double price;
+    //maybe have tags as an array list of strings?
+    public String tag;
+}
---
+++ IROM/app/src/main/java/com/sjsu/se195/irom/Profile.java
@@ -0,0 +1,41 @@
+package com.sjsu.se195.irom;
+
+/**
+ * Created by Krystle on 10/28/2016.
+ */
+
+public class Profile {
+    public  String uID;
+    public String firstName;
+    public  String lastName;
+    public String currency;
+    // TODO: add timezone, location
+
+    public Profile(){
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
