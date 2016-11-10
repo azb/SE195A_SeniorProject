9bfa53086fea901c4b0c9025d3a700e83b7383c1
azb <azbaney@gmail.com>
2016-11-09 19:34:09 -0800

    Added left swipe drawer, new activities

--- IROM/app/src/main/java/com/sjsu/se195/irom/InventoryActivity.java
+++ IROM/app/src/main/java/com/sjsu/se195/irom/InventoryActivity.java
@@ -1,8 +1,27 @@
 package com.sjsu.se195.irom;
 
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.SearchView;
+
 /**
  * Created by Arthur on 11/9/2016.
  */
 
-public class InventoryActivity {
+public class InventoryActivity extends NavigationDrawerActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    /*
+        SearchView searchView = (SearchView) menu.findItem(R.id.inventory_search).getActionView();
+        //searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+    */
+        return super.onCreateOptionsMenu(menu);
+
+    }
 }
