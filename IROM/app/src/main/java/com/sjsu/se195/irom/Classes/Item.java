20387fff2265aca8c9d3b4522f0e2c63abde227c
Krystle <krystleweinrich@yahoo.com>
2016-11-14 23:07:39 -0800

    manually update an item now!

--- IROM/app/src/main/java/com/sjsu/se195/irom/Classes/Item.java
+++ IROM/app/src/main/java/com/sjsu/se195/irom/Classes/Item.java
@@ -1,13 +1,16 @@
 package com.sjsu.se195.irom;
 
+import com.google.firebase.database.Exclude;
+
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Krystle on 10/28/2016.
  */
 
 public class Item {
-    public String itemID;
     public String uID;
     public Date dateAdded;
     public String name;
@@ -18,8 +21,15 @@ public class Item {
     public String tag;
     public Boolean isForSale;
 
-    public Item(String iid, String uid, Date d, String n, Integer q, String  note, String tag, Boolean forSale){
-        this.itemID = iid;
+    public String getuID() {
+        return uID;
+    }
+
+    public void setuID(String uID) {
+        this.uID = uID;
+    }
+
+    public Item(String uid, Date d, String n, Integer q, String  note, String tag, Boolean forSale){
         this.uID = uid;
         this.dateAdded = d;
         this.name = n;
@@ -30,6 +40,66 @@ public class Item {
     }
 
     public Item(){
+    }
 
+    public Date getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Boolean getForSale() {
+        return isForSale;
+    }
+
+    public void setForSale(Boolean forSale) {
+        isForSale = forSale;
+    }
+
+    @Exclude
+    public Map<String,Object> toMap(){
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("uID", uID);
+        result.put("dateAdded", dateAdded);
+        result.put("name",name);
+        result.put("notes",note);
+        result.put("tags",tag);
+        result.put("forSale",isForSale);
+
+        return result;
     }
 }
--- IROM/app/src/main/java/com/sjsu/se195/irom/ItemActivity.java
+++ IROM/app/src/main/java/com/sjsu/se195/irom/ItemActivity.java
@@ -1,7 +1,24 @@
 package com.sjsu.se195.irom;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This is to test adding an item to the database.
@@ -9,12 +26,88 @@ import android.widget.TextView;
  */
 
 public class ItemActivity extends NavigationDrawerActivity{
+    private ScrollView manAddItemForm;
+    private EditText mName;
+    private EditText mQuantity;
+    private EditText mNotes;
+    private DatabaseReference mDatabase;
+    private FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       TextView itemt = (TextView) findViewById(R.id.item_text);
+        //INSTEAD OF setContentView(R.layout.activity_welcome); USE NEXT 3 LINES IF YOU WANT TH NAV BAR TO WORK
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View contentView = inflater.inflate(R.layout.activity_item, null, false);
+        drawer.addView(contentView, 0);
 
+        //initialize things
+        Button manuallyAddItemButton = (Button) findViewById(R.id.manually_add_item_button);
+        manAddItemForm = (ScrollView) findViewById(R.id.manually_add_item_form);
+       Button submitItemButton = (Button) findViewById(R.id.submit_item_button);
+        mName = (EditText) findViewById(R.id.item_name_text);
+        mQuantity = (EditText) findViewById(R.id.item_quantity_text);
+        mNotes = (EditText) findViewById(R.id.item_note_text);
+
+        //get current user
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        //get database reference
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        //button click functions
+        manuallyAddItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (manAddItemForm.getVisibility()== View.VISIBLE){
+                    //if visible make invisible
+                    mName.setText("");
+                    mNotes.setText("");
+                    mQuantity.setText("");
+                    manAddItemForm.setVisibility(View.INVISIBLE);
+                }else{
+                    //it is invisible so make visible
+                    manAddItemForm.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        submitItemButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                //check fields
+                if(validityCheck()){
+                    //manually insert items. TODO make this a constructor.
+                    //TODO figure out iid
+                    com.sjsu.se195.irom.Item newItem = new com.sjsu.se195.irom.Item(
+                            mUser.getUid(),
+                            new Date(),
+                            mName.getText().toString(),
+                            Integer.getInteger(mQuantity.getText().toString()),
+                            mNotes.getText().toString(),
+                            "thisisatag",
+                            false);
+                    writeNewManualItem(newItem);
+                }
+                else{
+                    Toast.makeText(ItemActivity.this, "something wasnt filled. something broke", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void writeNewManualItem(com.sjsu.se195.irom.Item i) {
+        Map<String, Object> data = new HashMap<>();
+        String key = mDatabase.child("items").push().getKey();
+        data.put(key,i);
+
+        mDatabase.child("items").updateChildren(data);
+    }
+
+    private Boolean validityCheck() {
+        if (mName==null || mQuantity == null || mNotes == null){
+            mNotes.setError("fill all fields");
+            return false;
+        }
+        return true;
     }
 
 
