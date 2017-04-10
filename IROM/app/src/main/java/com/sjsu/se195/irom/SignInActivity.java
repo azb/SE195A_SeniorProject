af3381419f659ce93cd5acb1456491674c75abcc
Krystle <krystleweinrich@yahoo.com>
2017-04-09 23:13:13 -0700

    keyboard defocuses when not editing an edit text!

--- IROM/app/src/main/java/com/sjsu/se195/irom/SignInActivity.java
+++ IROM/app/src/main/java/com/sjsu/se195/irom/SignInActivity.java
@@ -1,8 +1,11 @@
 package com.sjsu.se195.irom;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.text.TextUtilsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -11,11 +14,14 @@ import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.vision.text.Text;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -29,9 +35,12 @@ public class SignInActivity extends AppCompatActivity {
     private Button signInButton;
     private EditText email;
     private EditText password;
+    private TextView forgotPassword;
+    private TextView signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
         //figure out later if we want toolbar in activity
@@ -59,6 +68,16 @@ public class SignInActivity extends AppCompatActivity {
 
         //set up edit texts (email and password fields)
         email = (EditText) findViewById(R.id.signin_email);
+        //if focus isnt on email, close kb
+        email.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(view);
+                }
+            }
+        });
         //when you change stuff, remove that error
         email.addTextChangedListener(new TextWatcher() {
 
@@ -106,6 +125,15 @@ public class SignInActivity extends AppCompatActivity {
             }
 
         });
+        password.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (!b) {
+                    hideKeyboard(view);
+                }
+            }
+        });
         //set up button and button click function
         signInButton = (Button) findViewById(R.id.signin_button);
         signInButton.setOnClickListener(new View.OnClickListener(){
@@ -125,6 +153,56 @@ public class SignInActivity extends AppCompatActivity {
 
 
                 }
+            }
+        });
+        //set up forgot password stuff
+        forgotPassword = (TextView) findViewById(R.id.forgot_pw_text);
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Dialog dialog = new Dialog(SignInActivity.this);
+                dialog.setContentView(R.layout.dialog_forgot_pw);
+                dialog.setCanceledOnTouchOutside(true);
+                dialog.setTitle("Password reset");
+
+                Button button = (Button) dialog.findViewById(R.id.dialog_ok);
+                button.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+
+                        EditText edit=(EditText) dialog.findViewById(R.id.forgot_email);
+                        String text=edit.getText().toString();
+                        if(android.util.Patterns.EMAIL_ADDRESS.matcher(text).matches()){
+                            mAuth.sendPasswordResetEmail(text)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+
+                                                Log.d("forgotpwdialog", "Email sent.");
+                                            }
+                                        }
+                                    });
+                            Toast.makeText(SignInActivity.this, "Password request sent to "+text+".", Toast.LENGTH_SHORT).show();
+                            dialog.dismiss();
+                        }
+
+
+                    }
+                });
+
+
+                dialog.show();
+
+            }
+        });
+        //set up sign up link
+        signUp = (TextView) findViewById(R.id.sign_up_text);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent( getBaseContext(), SignUpActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -167,4 +245,9 @@ public class SignInActivity extends AppCompatActivity {
         });
     }
 
+//this is used for hiding the keyboard
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
 }
